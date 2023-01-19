8df2c2d24397914db495ce92078856c9265544f5
Amadeu Serras <118369464+nightrunner4@users.noreply.github.com>
2023-01-19 23:59:26 +0100

    Improve themes functionality
    Now the button of selected theme has a border around it when it's
    selected.

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
-const blue = document.querySelector('.blue');
-const green = document.querySelector('.green');
-const orange = document.querySelector('.orange');
+const blue = document.getElementById('blue');
+const green = document.getElementById('green');
+const orange = document.getElementById('orange');
 const body = document.querySelector('body');
 const themes = [blue, green, orange];
 
 for (let theme in themes) {
-    themes[theme].addEventListener('click', function() {
-        let color = event.target.className;
-        body.style.background = `var(--${color}-gradient)`;
+    let color = themes[theme]; // blue, green, orange HTML divs
+    color.addEventListener('click', function() {
+        body.style.background = `var(--${color.id}-gradient)`; //color.id is simply a string
+
+        // remove previous borders
+        for (let i = 0; i < themes.length; i++) {
+            themes[i].classList.remove('theme-border'); 
+        };
+
+        color.classList.add('theme-border');
     })
 };
