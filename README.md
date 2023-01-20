2433fdc9cff5175d8165fc213fa735a60a750ccc
Amadeu Serras <118369464+nightrunner4@users.noreply.github.com>
2023-01-20 12:58:50 +0100

    Make theme buttons static
    Now, when switching between different themes, the position of the
    buttons won't change based on which one is selected. This is thanks
    to using a shadow emulating an outer border instead of a border, since
    the shadown does't add size, the position of the grid children stays
    completely still.

--- README.md
+++ README.md
@@ -4,6 +4,9 @@ Sign up form 📃
 
 This page is currently being developed.
 
+When a theme button is clicked, a black outer border appears to indicate that it's been selected. Initially I was using a normal CSS border for this, but the position of the rest of the elements would 
+change slightly when one or other button was selected, since the size of the element would slightly increase and that would change the grid. The only way I could find around this problem is to use a shadow emulating a border, since the shadow, while being outside of the element, doesn't count on it's size. I'm sure there has to be a better solution, but for the moment I'm happy with this.
+
 Why are template literals so awesome?
 
 <b>Potential Improvements:</b> 💪
--- script.js
+++ script.js
@@ -2,21 +2,20 @@ const blue = document.getElementById('blue');
 const green = document.getElementById('green');
 const orange = document.getElementById('orange');
 const body = document.querySelector('body');
-const button = document.querySelector('button');
+const createAccount = document.getElementById('create-account');
 const themes = [blue, green, orange];
 
 for (let theme in themes) {
-    let color = themes[theme]; // blue, green, orange HTML divs
+    let color = themes[theme]; // blue, green & orange HTML circle buttons
     color.addEventListener('click', function() {
         body.style.background = `var(--${color.id}-gradient)`; //color.id is simply 'blue', 'green'...
-        button.style.background = `var(--${color.id}-solid)`;
+        createAccount.style.backgroundColor = `var(--${color.id}-solid)`;
 
-        // remove previous borders
+        // remove previous circle button's borders
         for (let i = 0; i < themes.length; i++) {
             themes[i].classList.remove('theme-border'); 
         };
 
         color.classList.add('theme-border');
-
     })
 };
